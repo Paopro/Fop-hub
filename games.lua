ff927117f5ee394ae09d70532990650114717c54
Paopro <112558188+Paopro@users.noreply.github.com>
2023-04-06 10:48:54 +0700

    Delete games.lua

--- games.lua
+++
@@ -1,5 +0,0 @@
-local GAMES = {
-  [10215650900] = {name="Rogue-Lineage-Khei", script="https://raw.githubusercontent.com/Paopro/Fop-hub/main/Minion?token=GHSAT0AAAAAACA4XV6BJ7UCAJUGG7CJW57WZBK3FEA", working=true}
-  [10046661315] = {name="Rogue-Lineage-Gaia", script="https://raw.githubusercontent.com/Paopro/Fop-hub/main/Minion?token=GHSAT0AAAAAACA4XV6BJ7UCAJUGG7CJW57WZBK3FEA", working=true}
-}
-return GAMES
